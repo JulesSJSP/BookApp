083b5fec610c506e36d87946e3f862f1e5dbf88a
julesSJSP <jules.beausaert@jopie.be>
2025-02-25 11:34:38 +0100

    Initial Commit

---
+++ xcode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xcode
+//
+//  Created by chapter 2 on 25/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xcode/xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodeApp.swift
+//  xcode
+//
+//  Created by chapter 2 on 25/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
